186ead488a92bd61ccb1c63dc83d03f44f317534
Codebling <codeblingx@gmail.com>
2025-09-25 20:43:59 -0400

    Rename type

--- backend/modules/lockable_lru/main.go
+++ backend/modules/lockable_lru/main.go
@@ -15,7 +15,7 @@ import (
 	cmap "github.com/orcaman/concurrent-map/v2"
 )
 
-type LLRU[K cmap.Stringer, V any] struct {
+type threadunsafeLLRU[K cmap.Stringer, V any] struct {
 	unlocked         *lru.Cache[K, V]							//unlocked k-v store whose values can be evicted when a new value is added
 	locked						 cmap.ConcurrentMap[K, V]   //locked k-v store, whose values can never be evicted
 	size int			                                //total size, combined locked and unlocked
@@ -28,20 +28,20 @@ type Entry[K cmap.Stringer, V any] struct {
 }
 
 // New creates an LRU of the given size.
-func New[K cmap.Stringer, V any](size int) (*LLRU[K, V], error) {
+func New[K cmap.Stringer, V any](size int) (*threadunsafeLLRU[K, V], error) {
 	return NewWithEvict[K, V](size, nil)
 }
 
 // NewWithEvict constructs a fixed size cache with the given eviction
 // callback.
-func NewWithEvict[K cmap.Stringer, V any](size int, onEvicted func(key K, value V)) (*LLRU[K, V], error) {
+func NewWithEvict[K cmap.Stringer, V any](size int, onEvicted func(key K, value V)) (*threadunsafeLLRU[K, V], error) {
 	lru, err := lru.NewWithEvict(size, onEvicted)
 	if err != nil {	
 		return nil, err
 	}
 
 	m := cmap.NewStringer[K, V]()
-	llru := LLRU[K, V]{
+	llru := threadunsafeLLRU[K, V]{
 		unlocked: lru,
 		locked: m,
 		size: size,
@@ -51,7 +51,7 @@ func NewWithEvict[K cmap.Stringer, V any](size int, onEvicted func(key K, value 
 }
 
 // Add adds an unlocked value to the cache. If a value was evicted, returns it.
-func (llru *LLRU[K, V]) addUnlockedWithoutLockingNorCheckingCapacity(key K, value V) (*Entry[K, V]) {
+func (llru *threadunsafeLLRU[K, V]) addUnlockedWithoutLockingNorCheckingCapacity(key K, value V) (*Entry[K, V]) {
 	oldestKey, oldestValue, _ := llru.unlocked.GetOldest() //we can ignore the last parameter, which is false if the lru is empty
 	wasEvicted := llru.unlocked.Add(key, value)
 
@@ -65,7 +65,7 @@ func (llru *LLRU[K, V]) addUnlockedWithoutLockingNorCheckingCapacity(key K, valu
 // Add adds an unlocked value to the cache. 
 // If the value exists, it is updated. If it existed and was locked, it is unlocked.
 // Returns `false, nil` if there was no room, otherwise returns true and the evicted entry, if any
-func (llru *LLRU[K, V]) AddOrUpdateUnlocked(key K, value V) (ok bool, evicted *Entry[K, V]) {
+func (llru *threadunsafeLLRU[K, V]) AddOrUpdateUnlocked(key K, value V) (ok bool, evicted *Entry[K, V]) {
 	llru.lock.Lock()
 	defer llru.lock.Unlock()
 	
@@ -87,7 +87,7 @@ func (llru *LLRU[K, V]) AddOrUpdateUnlocked(key K, value V) (ok bool, evicted *E
 // Add adds a locked value to the cache. 
 // If the value exists, it is updated. If it existed and was unlocked, it is locked.
 // Returns `false, nil` if there was no room, otherwise returns true and the evicted entry, if any
-func (llru *LLRU[K, V]) AddOrUpdateLocked(key K, value V) (ok bool, evicted *Entry[K, V]) {
+func (llru *threadunsafeLLRU[K, V]) AddOrUpdateLocked(key K, value V) (ok bool, evicted *Entry[K, V]) {
 	llru.lock.Lock()
 	defer llru.lock.Unlock()
 	
@@ -105,7 +105,7 @@ func (llru *LLRU[K, V]) AddOrUpdateLocked(key K, value V) (ok bool, evicted *Ent
 
 // Contains checks if a key is in the cache, without updating the
 // recent-ness or deleting it for being stale.
-func (llru *LLRU[K, V]) Contains(key K) bool {
+func (llru *threadunsafeLLRU[K, V]) Contains(key K) bool {
 	c.lock.RLock()
 	containKey := c.lru.Contains(key)
 	c.lock.RUnlock()
@@ -114,7 +114,7 @@ func (llru *LLRU[K, V]) Contains(key K) bool {
 
 // Peek returns the key value (or undefined if not found) without updating
 // the "recently used"-ness of the key.
-func (llru *LLRU[K, V]) Peek(key K) (value V, ok bool) {
+func (llru *threadunsafeLLRU[K, V]) Peek(key K) (value V, ok bool) {
 	c.lock.RLock()
 	value, ok = c.lru.Peek(key)
 	c.lock.RUnlock()
@@ -124,7 +124,7 @@ func (llru *LLRU[K, V]) Peek(key K) (value V, ok bool) {
 // ContainsOrAdd checks if a key is in the cache without updating the
 // recent-ness or deleting it for being stale, and if not, adds the value.
 // Returns whether found and whether an eviction occurred.
-func (llru *LLRU[K, V]) ContainsOrAdd(key K, value V) (ok, evicted bool) {
+func (llru *threadunsafeLLRU[K, V]) ContainsOrAdd(key K, value V) (ok, evicted bool) {
 	var k K
 	var v V
 	c.lock.Lock()
@@ -147,7 +147,7 @@ func (llru *LLRU[K, V]) ContainsOrAdd(key K, value V) (ok, evicted bool) {
 // PeekOrAdd checks if a key is in the cache without updating the
 // recent-ness or deleting it for being stale, and if not, adds the value.
 // Returns whether found and whether an eviction occurred.
-func (llru *LLRU[K, V]) PeekOrAdd(key K, value V) (previous V, ok, evicted bool) {
+func (llru *threadunsafeLLRU[K, V]) PeekOrAdd(key K, value V) (previous V, ok, evicted bool) {
 	var k K
 	var v V
 	c.lock.Lock()
@@ -169,7 +169,7 @@ func (llru *LLRU[K, V]) PeekOrAdd(key K, value V) (previous V, ok, evicted bool)
 }
 
 // Remove removes the provided key from the cache.
-func (llru *LLRU[K, V]) Remove(key K) (present bool) {
+func (llru *threadunsafeLLRU[K, V]) Remove(key K) (present bool) {
 	var k K
 	var v V
 	c.lock.Lock()
@@ -186,7 +186,7 @@ func (llru *LLRU[K, V]) Remove(key K) (present bool) {
 }
 
 // Resize changes the cache size.
-func (llru *LLRU[K, V]) Resize(size int) (evicted int) {
+func (llru *threadunsafeLLRU[K, V]) Resize(size int) (evicted int) {
 	var ks []K
 	var vs []V
 	c.lock.Lock()
@@ -205,7 +205,7 @@ func (llru *LLRU[K, V]) Resize(size int) (evicted int) {
 }
 
 // RemoveOldest removes the oldest item from the cache.
-func (llru *LLRU[K, V]) RemoveOldest() (key K, value V, ok bool) {
+func (llru *threadunsafeLLRU[K, V]) RemoveOldest() (key K, value V, ok bool) {
 	var k K
 	var v V
 	c.lock.Lock()
@@ -222,7 +222,7 @@ func (llru *LLRU[K, V]) RemoveOldest() (key K, value V, ok bool) {
 }
 
 // GetOldest returns the oldest entry
-func (llru *LLRU[K, V]) GetOldest() (key K, value V, ok bool) {
+func (llru *threadunsafeLLRU[K, V]) GetOldest() (key K, value V, ok bool) {
 	c.lock.RLock()
 	key, value, ok = c.lru.GetOldest()
 	c.lock.RUnlock()
@@ -230,7 +230,7 @@ func (llru *LLRU[K, V]) GetOldest() (key K, value V, ok bool) {
 }
 
 // Keys returns a slice of the keys in the cache, from oldest to newest.
-func (llru *LLRU[K, V]) Keys() []K {
+func (llru *threadunsafeLLRU[K, V]) Keys() []K {
 	c.lock.RLock()
 	keys := c.lru.Keys()
 	c.lock.RUnlock()
@@ -238,7 +238,7 @@ func (llru *LLRU[K, V]) Keys() []K {
 }
 
 // Values returns a slice of the values in the cache, from oldest to newest.
-func (llru *LLRU[K, V]) Values() []V {
+func (llru *threadunsafeLLRU[K, V]) Values() []V {
 	c.lock.RLock()
 	values := c.lru.Values()
 	c.lock.RUnlock()
@@ -246,7 +246,7 @@ func (llru *LLRU[K, V]) Values() []V {
 }
 
 // Len returns the number of items in the cache.
-func (llru *LLRU[K, V]) Len() int {
+func (llru *threadunsafeLLRU[K, V]) Len() int {
 	c.lock.RLock()
 	length := c.lru.Len()
 	c.lock.RUnlock()
