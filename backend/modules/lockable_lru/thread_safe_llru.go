6941b3c4f7fc054b8bbbdc47e1642f98665655ac
Codebling <codeblingx@gmail.com>
2025-09-25 20:56:02 -0400

    Add threadsafe version of locking LRU

---
+++ backend/modules/lockable_lru/thread_safe_llru.go
@@ -0,0 +1,61 @@
+package lockable_lru
+
+/*
+ * A thread-safe LRU implementation with items that can be "locked"
+ *
+ * Based on hashicorp/golang-lru
+ *
+ * A locked item cannot be evicted until it is unlocked. When it is unlocked, it is moved to the most recent.
+ *
+ */
+import (
+	"sync"
+
+	cmap "github.com/orcaman/concurrent-map/v2"
+)
+
+type LLRU[K cmap.Stringer, V any] struct {
+	llru ThreadunsafeLLRU[K, V]
+	lock sync.RWMutex //even though the underlying structures are threadsafe, we need to lock if we have to do 2 or more operations - which means we have to lock for every operation, otherwise we could deadlock if one call has locked the outer lock but is waiting on the inner lock, and another call has not locked the outer but has locked the inner
+}
+
+// New creates an LRU of the given size.
+func New[K cmap.Stringer, V any](size int) (*LLRU[K, V], error) {
+	llru, err := NewUnsafe[K, V](size)
+	if err != nil {
+		return nil, err
+	}
+	return &LLRU[K, V]{
+		llru: *llru,
+	}, nil
+}
+
+// NewWithEvict constructs a fixed size cache with the given eviction
+// callback.
+func NewWithEvict[K cmap.Stringer, V any](size int, onEvicted func(key K, value V)) (*LLRU[K, V], error) {
+	llru, err := NewUnsafeWithEvict(size, onEvicted)
+	if err != nil {
+		return nil, err
+	}
+	return &LLRU[K, V]{
+		llru: *llru,
+	}, nil
+}
+
+// Add adds an unlocked value to the cache.
+// If the value exists, it is updated. If it existed and was locked, it is unlocked.
+// Returns `false, nil` if there was no room, otherwise returns true and the evicted entry, if any
+func (llru *LLRU[K, V]) AddOrUpdateUnlocked(key K, value V) (ok bool, evicted *Entry[K, V]) {
+	llru.lock.Lock()
+	defer llru.lock.Unlock()
+	return llru.AddOrUpdateUnlocked(key, value)
+}
+
+// Add adds a locked value to the cache.
+// If the value exists, it is updated. If it existed and was unlocked, it is locked.
+// Returns `false, nil` if there was no room, otherwise returns true and the evicted entry, if any
+func (llru *LLRU[K, V]) AddOrUpdateLocked(key K, value V) (ok bool, evicted *Entry[K, V]) {
+	llru.lock.Lock()
+	defer llru.lock.Unlock()
+	return llru.AddOrUpdateLocked(key, value)
+}
